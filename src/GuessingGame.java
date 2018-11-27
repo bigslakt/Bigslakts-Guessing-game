f6ae4229a764e7fd809ea2353c17cb2bba041323
robin hansson <robin-hansson@hotmail.com>
2018-11-28 00:49:54 +0100

    Task 5 and game complete

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -2,10 +2,32 @@ public class GuessingGame {
 
     public static void main(String[] args) {
 
+        int low;
+        int high;
+
+        if(args.length > 1) {
+
+            low = Integer.parseInt(args[0]);
+            high = Integer.parseInt(args[1]);
+
+            System.out.println("Users interval:");
+        }
+        else
+        {
+            low = 0;
+            high = 1000;
+
+            System.out.println("Correct user arguments missing...");
+            System.out.println("Auto-generated interval:");
+        }
+
+        System.out.println(low + " " + high);
+        System.out.println();
+
         Guesser guesser = null;
 
         try {
-            guesser = new Guesser(0, 1000);
+            guesser = new Guesser(low, high);
         }
         catch(IllegalArgumentException iae)
         {
